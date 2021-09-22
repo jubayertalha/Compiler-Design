d399f5eb54e79bacb7c36dc0f2abb8b71034e25f
Talha Jubayer <jubayertalha194@gmail.com>
2021-09-22 23:55:37 +0600

    test

--- Mid_Lab_02/task_01_keyword.cpp
+++ Mid_Lab_02/task_01_keyword.cpp
@@ -22,7 +22,6 @@ bool check(string key){
         if(c == ' ' || c == ','){
           c = strLine[i++];
           continue;
-          //tt
         }else if(c == '\"'){
           if(!start){
             start = true;
