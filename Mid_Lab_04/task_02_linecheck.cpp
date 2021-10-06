fee9f9c10cbaf2aefc8fc70e87c1b76540f928a4
Talha Jubayer <jubayertalha194@gmail.com>
2021-10-06 16:21:39 +0600

    add mid lab 04 task 02 linecheckup

---
+++ Mid_Lab_04/task_02_linecheck.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string line;
+    getline(cin,line);
+
+    bool isValid = true;
+    int total = 0;
+
+    string word;
+    istringstream ss(line);
+    while(ss>>word){
+        for(int i=0; i<word.length(); i++){
+            char ch = word[i];
+            if(total==0){
+                if(i==0){
+                    if(ch>='A'&&ch<='Z'){
+                        continue;
+                    }else{
+                        isValid = false;
+                        break;
+                    }
+                }else{
+                    if((ch>='a'&&ch<='z')||'.'){
+                        continue;
+                    }else{
+                        isValid = false;
+                        break;
+                    }
+                }
+            }else{
+                if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')||'.'){
+                    continue;
+                }else{
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+        total++;
+    }
+    if(word[word.length()-1]!='.'){
+        isValid = false;
+    }
+    
+    if(isValid){
+        cout<<"Valid Statement"<<endl;
+    }else{
+        cout<<"Invalid Statement"<<endl;
+    }
+	return 0;
+}
